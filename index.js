9819f2be9ed100920c835e7aa95695f7b85f99c5
matteosourdes <matteo.sourdes19@gmail.com>
2022-11-04 21:05:33 +0100

    finalisation du html et du css et début du js

---
+++ index.js
@@ -0,0 +1,16 @@
+const imgDog = document.getElementById("img-dog")
+const imgCat = document.getElementById("img-cat")
+const buttonDog = document.getElementById("dog")
+const buttonCat = document.getElementById("cat")
+let verification
+
+buttonDog.addEventListener('click', () => {
+    verification = "dog"
+    console.log(verification)
+})
+
+
+function start(){
+    
+    fetch()
+}
