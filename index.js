f0fca6081917537fff58ab0048ec19d8be96833a
Noé Sourdès <souredesnoe19@gmail.com>
2022-11-04 23:37:47 +0100

    finalisation du projet

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const imgDog = document.getElementById("img-dog")
-const imgCat = document.getElementById("img-cat")
+img = document.getElementById("img")
+console.log(img)
 const buttonDog = document.getElementById("dog")
 const buttonCat = document.getElementById("cat")
 let verification
 
 buttonDog.addEventListener('click', () => {
     verification = "dog"
-    console.log(verification)
+
+})
+buttonCat.addEventListener('click', () => {
+    verification = "cat"
+
 })
 
-
 function start(){
-    
-    fetch()
+    if(verification==="dog"){
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => res.json())
+            .then(data => img.src = data.message)
+            img.style.display = "block"
+    }
+    if(verification==="cat"){
+        fetch("https://api.thecatapi.com/v1/images/search")
+            .then(res => res.json())
+            .then(data => img.src = data[0].url)
+            img.style.display = "block"
+    }
+    if(verification !== "cat" && verification !== "dog"){
+        alert("Vous n'avez pas sélectionné une catégorie !!")
+    }
 }
